d9db8d1f5110ff46b07d3aab750e21c8ced3ed44
Alexander Mykulych <alexander.mykulych@gmail.com>
2023-10-07 15:26:13 +0300

    chore

--- src/composables/useQuoridor.ts
+++ src/composables/useQuoridor.ts
@@ -1,5 +1,5 @@
 type QuoridorGame = {
-
+  board: Board
 }
 
 type QuoridorGameState = {
@@ -22,6 +22,47 @@ type Wall = {
   node4: Node
 }
 
-export function useQuoridor(): QuoridorGame {
-  const quoridorState = ref<QuoridorGameState>({})
+type Board = {
+  nodes: Node[]
+  walls: Wall[]
+}
+
+type QuoridorConfig = {
+  size: {
+    width: number
+    height: number
+  }
+}
+
+export function useQuoridor(config: QuoridorConfig = getDefaultQuoridorConfig()): QuoridorGame {
+  const quoridorState = ref<QuoridorGameState>({
+    
+  })
+
+
+}
+
+export function createBoard(config: QuoridorConfig): Board {
+  const nodes: Node[] = generateNodes(config)
+  
+  return {
+    nodes: [],
+    walls: []
+  }
+}
+
+export function generateNodes(config: QuoridorConfig): Node[] {
+  return Array(config.size.width)
+    .flatMap((_, x) => Array(config.size.height)
+      .map((_, y) => ({ x, y }))
+    )
+}
+
+export function getDefaultQuoridorConfig(): QuoridorConfig {
+  return {
+    size: {
+      width: 9,
+      height: 9
+    }
+  }
 }
